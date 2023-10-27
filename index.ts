532e42cb0a44c48a8262dc68b2186d476eee82b2
Felinto <emersong20.email@gmail.com>
2023-10-27 07:09:33 +0000

    refactor npm module

--- index.ts
+++ index.ts
@@ -9,14 +9,30 @@ interface ExtendedPage extends Page {
 
 export const screenshots: string[] = [];
 
-export const newPage = async () => {
-	puppeteerExtra.use(stealthPlugin());
-	puppeteerExtra.use(
-		RecaptchaPlugin({
-			provider: { id: '2captcha', token: process.env.TWO_CAPTCHA_KEY },
-			visualFeedback: true,
-		})
-	);
+puppeteerExtra.use(stealthPlugin());
+puppeteerExtra.use(
+	RecaptchaPlugin({
+		provider: { id: '2captcha', token: process.env.TWO_CAPTCHA_KEY },
+		visualFeedback: true,
+	})
+);
+
+const validateEnvironmentVariables = () => {
+	const requiredVars = [
+		'TWO_CAPTCHA_KEY',
+		'CHROME_HEADLESS_WS_URL',
+		'PROXY_USERNAME',
+		'PROXY_PASSWORD'
+	];
+	for (const variable of requiredVars) {
+		if (!process.env[variable]) {
+			throw new Error(`Environment variable ${variable} is missing.`);
+		}
+	}
+};
+
+export const newPage = async (): Promise<ExtendedPage> => {
+	validateEnvironmentVariables();
 
 	const browser = await puppeteerExtra.connect({
 		browserWSEndpoint: process.env.CHROME_HEADLESS_WS_URL,
@@ -27,8 +43,8 @@ export const newPage = async () => {
 	page.setDefaultNavigationTimeout(10 * 1000); // 10 seconds
 
 	await page.authenticate({
-		username: process.env.PROXY_USERNAME,
-		password: process.env.PROXY_PASSWORD,
+		username: process.env.PROXY_USERNAME!,
+		password: process.env.PROXY_PASSWORD!,
 	});
 
 	page.takeScreenshot = async () => {
@@ -37,4 +53,4 @@ export const newPage = async () => {
 	}
 
 	return page;
-}
+};
